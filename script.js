df23fe37fcf56f53b9719b5bba246220b4b55aa2
asama <asamahayder@yahoo.dk>
2024-01-26 21:53:59 +0100

    welcome page

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    const continueBtn = document.getElementById("continue-btn");
+
+    continueBtn.addEventListener("click", function () {
+        // Redirect to the portfolio page
+        window.location.href = "portfolio.html"; // Change this to your portfolio page name
+    });
+
     const imageItems = document.querySelectorAll(".image-item");
     const overlayContainer = document.getElementById("overlay-container");
     const enlargedImage = document.getElementById("enlarged-image");

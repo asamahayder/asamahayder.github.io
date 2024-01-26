8b6554306ab69d1325546743be0211d46b569781
asama <asamahayder@yahoo.dk>
2024-01-26 22:24:38 +0100

    split files

---
+++ script1.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const continueBtn = document.getElementById("continue-btn");
+
+    continueBtn.addEventListener("click", function () {
+        // Redirect to the portfolio page
+        window.location.href = "portfolio.html"; // Change this to your portfolio page name
+    });
+});
